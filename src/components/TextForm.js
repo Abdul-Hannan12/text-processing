7b2226f53adc9bb38d9091cd6f0332c1c8e6eab8
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-11-19 20:00:40 +0500

    initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TextForm(){
+    return (
+        <div className="container">
+            <input className="mainInput" type="text" placeholder="Enter Text Here"/>
+        </div>
+    );
+}
